de74d5aa830e9b2ab047295bab98631bd34e3936
Matheus <matheussjd9@gmail.com>
2024-02-12 21:50:03 -0300

    tests: Adicionando testes do CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,61 @@
-from fastapi.testclient import TestClient
-from app.app import app
+def test_root_deve_retornar_200_e_ola_mundo(client):
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_root_deve_retornar_200_e_ola_mundo():
-    client = TestClient(app) # Organizar (Arrange)
-    response = client.get('/') # Agir (Act)
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'matheus',
+            'email': 'matheus@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'matheus',
+        'email': 'matheus@example.com',
+        'id': 1,
+    }
 
-    assert response.status_code == 200 # Afirmar (Assert)
-    assert response.json() == {'message': 'Olá Mundo!'} # Afirmar (Assert)
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == 200
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'matheus',
+                'email': 'matheus@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'matheus',
+            'email': 'matheus@example.com',
+            'password': '123',
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        'username': 'matheus',
+        'email': 'matheus@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == 200
+    assert response.json() == {'message': 'User deleted'}
