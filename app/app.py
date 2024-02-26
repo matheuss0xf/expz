677a6e2d452636129b094d8c922af7bf3dcafd36
Matheus <matheussjd9@gmail.com>
2024-02-25 23:10:13 -0300

    feat: adicionando SQLAlchemy (ORM)

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
 # FastAPI + psycopg2 ou asyncpg + plain/raw SQL
 # 🐘Postgres
 # 🟩Nginx
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
-from app.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+from app.database import get_session
+from app.models import User
+from app.schemas import Message, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
@@ -13,46 +17,61 @@ def read_root():
     return {'message': 'Olá Mundo!'}
 
 
-database = []
+@app.post('/users/', response_model=UserPublic, status_code=201)
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(User.username == user.username)
+    )
+
+    if db_user:
+        raise HTTPException(
+            status_code=400, detail='Username already registered'
+        )
+
+    db_user = User(
+        username=user.username, password=user.password, email=user.email
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
-@app.post('/users/', status_code=201, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
-
-    database.append(user_with_id)
-
-    return user_with_id
-
-
-@app.get('/users/', status_code=200, response_model=UserList)
-def read_users():
-    return {'users': database}
+@app.get('/users/', response_model=UserList)
+def read_users(
+    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
+):
+    users = session.scalars(select(User).offset(skip).limit(limit)).all()
+    return {'users': users}
 
 
-@app.get('/users/{user_id}', status_code=200, response_model=UserPublic)
-def read_user(user_id: int):
-    if user_id > len(database) or user_id < 1:
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(
+    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+):
+
+    db_user = session.scalar(select(User).where(User.id == user_id))
+    if not db_user:
         raise HTTPException(status_code=404, detail='User not found')
 
-    return database[user_id - 1]
+    db_user.username = user.username
+    db_user.password = user.password
+    db_user.email = user.email
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
-@app.put('/users/{user_id}', status_code=200, response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema):
-    if user_id > len(database) or user_id < 1:
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+
+    if not db_user:
         raise HTTPException(status_code=404, detail='User not found')
 
-    user_with_id = UserDB(**user.model_dump(), id=user_id)
-    database[user_id - 1] = user_with_id
+    session.delete(db_user)
+    session.commit()
 
-    return user_with_id
-
-
-@app.delete('/users/{user_id}', status_code=200)
-def delete_user(user_id: int):
-    if user_id > len(database) or user_id < 1:
-        raise HTTPException(status_code=404, detail='User not found')
-
-    database.pop(user_id - 1)
     return {'message': 'User deleted'}
